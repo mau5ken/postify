6be5d5247d4d63c566deaa6c264ec30aab0e15a7
ken <harygato3@gmail.com>
2020-03-17 14:15:44 -0600

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,37 +2,28 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Postify: Expresa tu opinion al mundo"
-  end
-
-  test "should get root" do
-    ger static_pages_root_url
-    assert_response :success
-  end
-
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Postify: Expresa tu opinion al mundo"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | Postify: Expresa tu opinion al mundo"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "About | Postify: Expresa tu opinion al mundo"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", "Contact | Postify: Expresa tu opinion al mundo"
   end
 
 end
