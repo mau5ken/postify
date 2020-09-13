564bc6dffdf9c5433c4864d8c912fa72a4b35e3d
ken <harygato3@gmail.com>
2020-09-13 01:48:22 -0500

    added devise parameters for other columns in DB

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    
-    def hello
-        render html: "Hello, world!"
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sign_in_path_for(resource)
+        if resource.class == User
+            posts_path
+        end
     end
+
+    protected
+
+    def configure_permitted_parameters
+        edit_attribute = [:avatar]
+        register_attribute = [:name, :avatar, :gender]
+
+
+        devise_parameter_sanitizer.permit(:sign_up, keys: register_attribute)
+        devise_parameter_sanitizer.permit(:account_update, keys: edit_attribute)
+    end
+    
 end
